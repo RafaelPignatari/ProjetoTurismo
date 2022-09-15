ec879cb5a4b3cc49bc21ac3f2e30c616180b66c6
LucasLosano <losano3@hotmail.com>
2022-09-14 22:23:26 -0300

    bug fixes e tela de loading

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ let paises;
 let cidades = [];
 let questoes = [];
 
+function showLoadDiv(){
+    document.getElementById("loader").style.display = "block";
+}
+
+function limitaIdiomas(idiomas, limite){
+    while(idiomas.length > limite){
+        idiomas.splice(Math.floor(Math.random() * idiomas.length), 1);
+    }
+}
+
 async function getLanguageFromContinent() {
     paises = await getIBGECountryInfo();
     let paisesFiltroContinental;
@@ -13,12 +23,63 @@ async function getLanguageFromContinent() {
     let idiomas;
     
     paisesFiltroContinental = paises.filter((obj) => obj['localizacao']['regiao']['nome'] == continenteValue)
-    idiomas = getAllCountryLanguages(paisesFiltroContinental);
-    
+    idiomas = getAllCountryLanguages(paisesFiltroContinental).sort();
+    limitaIdiomas(idiomas, 15)
     return idiomas;
 }
 
-continente.addEventListener("change", async function() {
+botaoTestar.addEventListener("click", async function(event) {
+    event.preventDefault(); //Para que o botão não atualize a página de cara.
+    cidades = [];
+    let continenteEscolhido = document.querySelector("#continente > input:checked").value;
+    let idiomaEscolhido = document.querySelector("#idioma > input:checked").value;
+    let climaEscolhido = document.querySelector("#clima > input:checked").value;
+    let paisesFiltrados = paises.filter((pais) => pais['localizacao']['regiao']['nome'] == continenteEscolhido && pais['linguas'][0]['nome'] == idiomaEscolhido);
+    
+    showLoadDiv();
+    for (let i in paisesFiltrados) {
+        let codigoPais = paisesFiltrados[i].id["ISO-3166-1-ALPHA-2"];
+        let cidadesAPI = await getCitiesbyCountry(codigoPais);
+        
+        for(let number in cidadesAPI) {
+            clima = await getWheaterInfo(cidadesAPI[number]["name"]);
+            valorMonetario = await getMonetaryInfo(paisesFiltrados[i]["unidades-monetarias"][0].id["ISO-4217-ALPHA"]);
+            cidadesAPI[number]['temperatura'] = clima['temperatura'];
+            cidadesAPI[number]['pais'] = paisesFiltrados[i].nome.abreviado;
+            cidadesAPI[number]['historico'] = paisesFiltrados[i].historico;
+            cidadesAPI[number]['idioma'] = idiomaEscolhido;   
+            cidadesAPI[number]['moeda'] = valorMonetario;                      
+        }   
+        cidades = cidades.concat(cidadesAPI);
+    }; 
+    
+    // cidades.sort(function(a, b){
+    //     return b['temperatura'] - a['temperatura']
+    // })
+
+    // if (cidades.length < 4 ){console.log(cidades)}
+
+    // else if (climaEscolhido = 'calor'){
+    //     var test = cidades.slice(3)
+    //     console.log(test)
+    // }
+    // else if (climaEscolhido = 'ameno'){
+    //     var test = cidades.slice(Math.round(qtdCidades/2), Math.round(qtdCidades/2) + 3)
+    //     console.log(test)
+    // }
+    // else if (climaEscolhido = 'frio'){
+    //     var test = cidades.slice(-3)
+    //     console.log(test)
+    // }
+    
+    sessionStorage.setItem("cidades", JSON.stringify(cidades));
+    window.location.href = "../Views/opcoesDeViagem.html";
+})
+
+continente.addEventListener("change", async function(event) {
+    if(isChecked(event.currentTarget))
+        return;
+    
     let idiomasAPI = await getLanguageFromContinent();
     let label = document.createElement("label");
     let br = document.createElement("br");
@@ -50,57 +111,20 @@ continente.addEventListener("change", async function() {
     continente.hidden = true;
 })
 
-botaoTestar.addEventListener("click", async function(event) {
-    event.preventDefault(); //Para que o botão não atualize a página de cara.
-    cidades = [];
-    let continenteEscolhido = document.querySelector("#continente > input:checked").value;
-    let idiomaEscolhido = document.querySelector("#idioma > input:checked").value;
-    let climaEscolhido = document.querySelector("#clima > input:checked").value;
-    let paisesFiltrados = paises.filter((pais) => pais['localizacao']['regiao']['nome'] == continenteEscolhido && pais['linguas'][0]['nome'] == idiomaEscolhido);
-    
-    for (let i in paisesFiltrados) {
-        let codigoPais = paisesFiltrados[i].id["ISO-3166-1-ALPHA-2"];
-        let cidadesAPI = await getCitiesbyCountry(codigoPais);
-        
-        for(let number in cidadesAPI) {
-            clima = await getWheaterInfo(cidadesAPI[number]["name"]);
-            valorMonetario = await getMonetaryInfo(paisesFiltrados[i]["unidades-monetarias"][0].id["ISO-4217-ALPHA"]);
-            cidadesAPI[number]['temperatura'] = clima['temperatura'];
-            cidadesAPI[number]['pais'] = paisesFiltrados[i].nome.abreviado;
-            cidadesAPI[number]['historico'] = paisesFiltrados[i].historico;
-            cidadesAPI[number]['idioma'] = idiomaEscolhido;   
-            cidadesAPI[number]['moeda'] = valorMonetario;                      
-        }   
-        cidades = cidades.concat(cidadesAPI);
-    }; 
-    
-    cidades.sort(function(a, b){
-        return b['temperatura'] - a['temperatura']
-    })
-    // let qtdCidades = cidades.length;
-    
-    // if (qtdCidades < 4 ){console.log(cidades)}
-    
-    // else if (climaEscolhido = 'calor'){
-    //     console.log(cidades.slice(3))
-    // }
-    // else if (climaEscolhido = 'ameno'){
-    //     console.log(cidades.slice(Math.round(qtdCidades/2), Math.round(qtdCidades/2) + 3))
-    // }
-    // else if (climaEscolhido = 'frio'){
-    //     console.log(cidades.slice(-3))
-    // }
-    
-    sessionStorage.setItem("cidades", JSON.stringify(cidades));
-    window.location.href = "../Views/opcoesDeViagem.html";
-})
-
-idioma.addEventListener("click", function() {
+idioma.addEventListener("click", function(event) {
+    if(isChecked(event.currentTarget))
+        return;
     idioma.hidden = true;
     dinheiro.hidden = false;
 })
 
-dinheiro.addEventListener("click", function() {
+dinheiro.addEventListener("click", function(event) {
+    if(isChecked(event.currentTarget))
+        return;
     dinheiro.hidden = true;
     clima.hidden = false;
 })
+
+function isChecked(div){
+    return div.querySelector("input:checked") === null;
+}
