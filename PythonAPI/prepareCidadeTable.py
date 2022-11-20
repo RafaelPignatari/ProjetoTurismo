6fb8ce56abd590144c33d5193fc23d512e36d94a
LucasLosano <losano3@hotmail.com>
2022-11-20 00:29:45 -0300

    Python API updates

---
+++ PythonAPI/prepareCidadeTable.py
@@ -0,0 +1,59 @@
+import requests
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database=" mydatabase"
+)
+mycursor = mydb.cursor()
+
+def insertValue(table, values):
+    sql = "INSERT INTO " + table +" (nome, pais, latitude, longitude, populacao) VALUES (%s, %s, %s, %s, %s)"
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+def getAllFromTableById(table, nome , pais):
+    mycursor.execute("SELECT * FROM " + table + " WHERE nome = %s AND pais = %s", (nome, pais))
+    myresult = mycursor.fetchone()
+    return myresult
+
+def getAllFromTable():
+    mycursor.execute("SELECT iso FROM pais")
+    myresult = mycursor.fetchall()
+    return myresult
+
+paises = getAllFromTable()
+for pais in paises:
+    headers = {'Accept': 'application/json', 'X-Api-Key': 'ltGCHBdbFL/PPXQTBlyBww==FxmgaTRlWM5qZXAq'}
+    
+    count = 0;
+    while(True):
+        x = requests.get('https://api.api-ninjas.com/v1/city?limit=30&country=' + pais[0], headers=headers)
+        if(x.status_code == 200):
+            count = 0
+            break
+
+        count = count + 1
+        if(count == 5):
+            break
+
+    cidades = x.json()
+    for cidade in cidades:
+        if (cidade.get("population") is None):
+            continue
+
+        nome = cidade["name"]
+        pais = cidade["country"]
+
+        result = getAllFromTableById("cidade", nome, pais)
+        if(result is not None):
+            continue
+        latitude = cidade["latitude"]
+        longitude = cidade["longitude"]
+        populacao = cidade["population"]
+        
+        
+        insertValue("cidade", (nome, pais, latitude, longitude, populacao))
+
