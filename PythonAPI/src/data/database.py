4cbb2c9d24b0aeed299be4cc8827c5aa80f7c911
LucasLosano <losano3@hotmail.com>
2022-11-21 23:54:30 -0300

    Closing MySQL connection

--- PythonAPI/src/data/database.py
+++ PythonAPI/src/data/database.py
@@ -1,34 +1,32 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="turistando-python-database.mysql.database.azure.com",
-    user="roott@turistando-python-database",
-    password="Turistando7@",
-    database="pythonapi",
-    client_flags= [mysql.connector.ClientFlag.SSL],
-    ssl_ca= "data\DigiCertGlobalRootG2.crt.pem"
-)
+config = {
+  'host':'turistando-python-database.mysql.database.azure.com',
+  'user':'roott@turistando-python-database',
+  'password':'Turistando7@',
+  'database':'pythonapi',
+  'client_flags': [mysql.connector.ClientFlag.SSL]
+}
+
+# Construct connection string
+mydb = mysql.connector.connect(**config)
 mycursor = mydb.cursor()
 
 def getContinente():
-    mycursor.execute("SELECT DISTINCT(continente) FROM pais;")
-    myresult = mycursor.fetchall()
-    return myresult
+    query = "SELECT DISTINCT(continente) FROM pais;"
+    return executeQuery(query)
 
 def getIdiomasByContinente(continente):
-    mycursor.execute("SELECT DISTINCT(l.nome) FROM paisLingua pl" 
-    + " INNER JOIN pais p ON pl.paisId = p.iso"
-    + " INNER JOIN lingua l ON pl.linguaId = l.id"
-    + " WHERE continente = %s;", [continente])
-    myresult = mycursor.fetchall()
-    return myresult
+    query = "SELECT DISTINCT(l.nome) FROM paisLingua pl INNER JOIN pais p ON pl.paisId = p.iso INNER JOIN lingua l ON pl.linguaId = l.id WHERE continente = %s;"
+    return executeQuery(query,[continente])
 
 def getCidades(continente, idioma):
-    mycursor.execute("SELECT c.nome, p.nome,l.nome, p.moeda, t.quartoTrimestre, p.historico FROM cidade c" 
-    + " INNER JOIN pais p ON p.iso = c.pais"
-    + " INNER JOIN paislingua pl ON pl.paisId = p.iso"
-    + " INNER JOIN lingua l ON l.id = pl.linguaId"
-    + " INNER JOIN temperaturaMediaCidade t ON c.id = t.cidadeId"
-    + " WHERE p.continente = %s AND l.nome = %s", [continente, idioma])
+    query = "SELECT c.nome, p.nome,l.nome, p.moeda, t.quartoTrimestre, p.historico FROM cidade c INNER JOIN pais p ON p.iso = c.pais INNER JOIN paislingua pl ON pl.paisId = p.iso INNER JOIN lingua l ON l.id = pl.linguaId INNER JOIN temperaturaMediaCidade t ON c.id = t.cidadeId WHERE p.continente = %s AND l.nome = %s;"
+    return executeQuery(query,[continente, idioma])
+
+def executeQuery(query, values = []):
+    mydb = mysql.connector.connect(**config)
+    mycursor.execute(query, values)
+    mydb.close()
     myresult = mycursor.fetchall()
     return myresult
